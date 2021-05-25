51708054e1eae708ab6b25d598c1a89101ab3e8a
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-25 14:34:03 +0000

    Brew formula update for couture version v0.0.10

--- Formula/couture.rb
+++ Formula/couture.rb
@@ -5,24 +5,24 @@
 class Couture < Formula
   desc "Allows for tailing multiple event sources."
   homepage ""
-  version "0.0.9"
+  version "0.0.10"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/gaggle-net/couture/releases/download/v0.0.9/couture_0.0.9_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "05da100dc74ef6709ec4120939ea4b101a2dc2c44a86e67c34bb9be45333780d"
+    url "https://github.com/gaggle-net/couture/releases/download/v0.0.10/couture_0.0.10_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "9c976e7f11337781903f426ea252581585dcfcd43a00956898cccd70bddeb4dd"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/gaggle-net/couture/releases/download/v0.0.9/couture_0.0.9_Darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "54c231f2d598984782c4c9c01e846d51ac9e566231c0e4ef4462ac82fbe4a10b"
+    url "https://github.com/gaggle-net/couture/releases/download/v0.0.10/couture_0.0.10_Darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "fd0e192e37d92d378fdecf9ca51788ec307717897d50be2695d16707e01a18cb"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/gaggle-net/couture/releases/download/v0.0.9/couture_0.0.9_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "a89457d90d78d142c841f78299b8d5b5611b0699f890656e3f87a90e05fa018b"
+    url "https://github.com/gaggle-net/couture/releases/download/v0.0.10/couture_0.0.10_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "ded139591fef79478651131e1c4ce009027da1572f7705067edbca21b131b286"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/gaggle-net/couture/releases/download/v0.0.9/couture_0.0.9_Linux_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "5dcb5dcdb9e262df5c33aa5473d90083adb5ffa6b10bf02e22f068ba8723531e"
+    url "https://github.com/gaggle-net/couture/releases/download/v0.0.10/couture_0.0.10_Linux_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "4c875e0b7d9d1e339817d3561f584d75bb2b6b5e0a983a611315a15a32dc11eb"
   end
 
   def install
