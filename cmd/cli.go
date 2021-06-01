d20d1ab72f3a8766e13ba087925621c32c4c2d22
Stephen Pandich <spandich@gaggle.net>
2021-05-31 19:15:52 -0700

    todo

--- cmd/cli.go
+++ cmd/cli.go
@@ -22,16 +22,17 @@ var cli struct {
 	AutoResize bool `group:"terminal" help:"Auto-resize columns when the terminal resizes." negatable:"true" default:"true"`
 
 	Theme            string `group:"display" help:"Specify the core Theme color: ${enum}." placeholder:"Theme" default:"${defaultTheme}" enum:"${themeNames}" env:"COUTURE_THEME"`
-	Multiline        bool   `group:"display" help:"Display each log event in multiline format." negatable:"true" default:"false"`
+	Multiline        bool   `group:"display" help:"Display each log event in multiline format. (Enabled by --expand-json)" negatable:"true" default:"false" xor:"line"`
 	Highlight        bool   `group:"display" help:"Highlight matches from the patterns specified in --include." negatable:"true" default:"true"`
 	ConsistentColors bool   `group:"display" help:"Maintain consistent source URL colors between runs." negatable:"true" default:"true"`
-	ExpandJSON       bool   `group:"display" help:"Example JSON message bodies. Warning: has a significant performance impact." negatable:"true" default:"false"`
+	ExpandJSON       bool   `group:"display" help:"Example JSON message bodies. Warning: has a significant performance impact." negatable:"true" default:"false" xor:"line"`
 
 	Column     []string   `group:"content" help:"Specify one or more columns to display: ${enum}." placeholder:"column" enum:"${columnNames}" env:"COUTURE_DEFAULT_COLUMN_NAMES"`
 	TimeFormat timeFormat `group:"content" help:"Go-standard time format string or a named format: ${timeFormatNames}." short:"t" default:"stamp" env:"COUTURE_DEFAULT_TIME_FORMAT"`
 
-	Level   level.Level     `group:"filter" help:"The minimum log level to display: ${enum}." default:"${defaultLogLevel}" placeholder:"level" short:"l" enum:"${logLevels}" env:"COUTURE_DEFAULT_LEVEL"`
-	Since   time.Time       `group:"filter" help:"How far back to look for events. Parses most time and duration formats including human friendly." placeholder:"(time|duration)" short:"s" default:"15m" env:"COUTURE_DEFAULT_SINCE"`
+	Level level.Level `group:"filter" help:"The minimum log level to display: ${enum}." default:"${defaultLogLevel}" placeholder:"level" short:"l" enum:"${logLevels}" env:"COUTURE_DEFAULT_LEVEL"`
+	Since time.Time   `group:"filter" help:"How far back to look for events. Parses most time and duration formats including human friendly." placeholder:"(time|duration)" short:"s" default:"15m" env:"COUTURE_DEFAULT_SINCE"`
+	// TODO include/exclude logic might be flawed. Process them one by one in order on command line, and short-circuit on first true
 	Include []regexp.Regexp `group:"filter" help:"Include filter regular expressions; they are performed before excludes." placeholder:"regex" short:"i" sep:"|"`
 	Exclude []regexp.Regexp `group:"filter" help:"Exclude filter regular expressions; they are performed after includes." placeholder:"regex" short:"x" sep:"|"`
 
