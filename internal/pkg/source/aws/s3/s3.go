8776477210769a9cf3bfb0e9a026312ba77f215d
Stephen Pandich <spandich@gaggle.net>
2021-06-03 17:17:52 -0700

    s3 fix

--- internal/pkg/source/aws/s3/s3.go
+++ internal/pkg/source/aws/s3/s3.go
@@ -10,7 +10,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	errors2 "github.com/pkg/errors"
 	"reflect"
-	"strings"
 	"sync"
 )
 
@@ -33,7 +32,9 @@ func Metadata() source.Metadata {
 // s3Source a S3 log poller.
 type s3Source struct {
 	aws.Source
-	s3 *s3.Client
+	s3     *s3.Client
+	bucket string
+	key    string
 }
 
 // newSource S3 source.
@@ -45,6 +46,8 @@ func newSource(sourceURL model.SourceURL) (*source.Source, error) {
 	src := s3Source{
 		Source: awsSource,
 		s3:     s3.NewFromConfig(awsSource.Config()),
+		bucket: sourceURL.Host,
+		key:    sourceURL.Path[1:],
 	}
 	var p source.Source = &src
 	return &p, nil
@@ -58,60 +61,22 @@ func (src *s3Source) Start(
 	_ chan model.SinkEvent,
 	errChan chan source.Error,
 ) error {
-	writer := chanWriteAt{
-		src:  src,
-		out:  srcChan,
-		lock: sync.Mutex{},
-	}
-	downloader := manager.NewDownloader(src.s3, func(d *manager.Downloader) {
-		const partSize = 16 * 1024
-		d.PartSize = partSize
-		d.Concurrency = 1
-	})
-	bucket := src.URL().Host
-	key := src.URL().Path[1:]
+	const partSize = 16 * 1024
+
 	go func() {
 		defer wg.Done()
+		writer := source.NewChanWriterAt(src, srcChan)
+		downloader := manager.NewDownloader(src.s3, func(d *manager.Downloader) {
+			d.PartSize = partSize
+			d.Concurrency = 1
+		})
+		request := &s3.GetObjectInput{Bucket: &src.bucket, Key: &src.key}
 		for running() {
-			if _, err := downloader.Download(context.TODO(), &writer, &s3.GetObjectInput{
-				Bucket: &bucket,
-				Key:    &key,
-			}); err != nil {
-				errChan <- source.Error{
-					SourceURL: src.URL(),
-					Error:     err,
-				}
+			// TODO context.Background()? interrupt it when running() goes false?
+			if _, err := downloader.Download(context.TODO(), writer, request); err != nil {
+				errChan <- source.Error{SourceURL: src.URL(), Error: err}
 			}
 		}
 	}()
 	return nil
-}
-
-type chanWriteAt struct {
-	src       source.Source
-	out       chan source.Event
-	remainder string
-	lock      sync.Mutex
-}
-
-// WriteAt ...
-func (b *chanWriteAt) WriteAt(p []byte, _ int64) (n int, err error) {
-	b.lock.Lock()
-	defer b.lock.Unlock()
-	if len(p) == 0 {
-		return 0, nil
-	}
-	s := b.remainder + string(p)
-	b.remainder = ""
-	var pieces = strings.Split(s, "\n")
-	if s[len(s)-1] != '\n' {
-		pieces, b.remainder = pieces[0:len(pieces)-1], pieces[len(pieces)-1]
-	}
-	for _, s := range pieces {
-		b.out <- source.Event{
-			Source: b.src,
-			Event:  s,
-		}
-	}
-	return len(p), nil
 }
---
+++ internal/pkg/source/writer.go
@@ -0,0 +1,48 @@
+package source
+
+import (
+	"io"
+	"strings"
+	"sync"
+)
+
+// NewChanWriterAt ...
+func NewChanWriterAt(
+	src Source,
+	out chan Event,
+) io.WriterAt {
+	return &chanWriteAt{
+		src:  src,
+		out:  out,
+		lock: sync.Mutex{},
+	}
+}
+
+// chanWriteAt ...
+type chanWriteAt struct {
+	src       Source
+	out       chan Event
+	remainder string
+	lock      sync.Mutex
+}
+
+// WriteAt ...
+func (writer *chanWriteAt) WriteAt(buf []byte, _ int64) (n int, err error) {
+	if len(buf) == 0 {
+		return 0, nil
+	}
+	writer.lock.Lock()
+	defer writer.lock.Unlock()
+
+	s := writer.remainder + string(buf)
+	writer.remainder = ""
+	var pieces = strings.Split(s, "\n")
+
+	if !strings.HasSuffix(s, "\n") {
+		pieces, writer.remainder = pieces[0:len(pieces)-1], pieces[len(pieces)-1]
+	}
+	for _, s := range pieces {
+		writer.out <- Event{Source: writer.src, Event: s}
+	}
+	return len(buf), nil
+}
